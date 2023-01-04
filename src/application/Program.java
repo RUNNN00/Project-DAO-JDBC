b6db0ab3c5468c905d19135fa73c3c81a07eba05
ruanazevedo <ruann.azevedo@outlook.com>
2023-01-03 22:24:16 -0300

    seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,16 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
-		
+
 		Department obj = new Department(1, "Books");
-		System.out.println(obj.toString());
+		Seller seller = new Seller(21, "Rob", "Rob@gmail.com", new Date(), 2000.00, obj);
+		System.out.println(seller);
 	}
 }
